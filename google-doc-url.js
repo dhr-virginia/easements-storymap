007446c910e6909683b02b6a625fb6ada16928e9
Jolene Smith <aejolene@users.noreply.github.com>
2019-03-25 19:00:00 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/10jk1Lwz5_9hhPS-u430iXKueKshpg6FzChIsFSeu4-s/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/10jk1Lwz5_9hhPS-u430iXKueKshpg6FzChIsFSeu4-s/edit#gid=0';
